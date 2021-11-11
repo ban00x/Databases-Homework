0c1d87c5d05a5f5eb8c13de49fc317a447613535
¨ban00x¨ <houssam.r321@gmail.com>
2021-11-12 00:08:34 +0100

    completed homework

---
+++ week-2/mandatory/api/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+
+const connection = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "Migracode",
+  port: "5432",
+});
+app.get("/customers", (req, res) => {
+  connection.query("select * from customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.get("/suppliers", (req, res) => {
+  connection.query("select * from suppliers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.get("/products", (req, res) => {
+  connection.query("select * from products", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.get("/orders", (req, res) => {
+  connection.query("select * from orders", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.get("/order_items", (req, res) => {
+  connection.query("select * from order_items", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+const port = 6000;
+app.listen(port, () =>
+  console.log(`Server is listening on ${port}. Ready to accept requests!`)
+);
